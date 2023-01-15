4dd7f2ce5a5803f3fdb50ea59df6e34dcf78a382
karasu yami <demigodlinty@gmail.com>
2023-01-15 04:39:41 -0600

    all index.js are properly working pushing changes soon

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,3 +1,12 @@
-const router = require('express').Router
+/* eslint-disable linebreak-style */
+const router = require('express').Router();
 
-const userRoutes = require
+const userRoutes = require('./userRoutes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
+router.use('/post', postRoutes);
+
+module.exports = router;
